b28410735a622fc221dbe048f86410c4fa854d6c
JoaoVargas <jotaguivargas@gmail.com>
2024-06-03 14:28:56 -0300

    trabalho JUnit

---
+++ Disciplinas/2024.1/2024.1.SOFT/Atividade Testes JUnit/Testes/tests/TestCartas.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestCartas {
+    @Test
+    public void TestDraw(){
+
+//        Assert.assertTrue();
+    }
+
+}
