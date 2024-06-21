d16feccb955fc93096179d36be966e9d235c2e59
JoaoVargas <jotaguivargas@gmail.com>
2024-06-20 23:59:41 -0300

    foi

---
+++ Disciplinas/2024.1/2024.1.SOFT/Trabalho Final/SistemaFumas/client/index.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './src/assets/index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './src/App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ Disciplinas/2024.1/2024.1.SOFT/Trabalho Final/SistemaFumas/client/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+  return (
+    <div className="bg-dark" data-bs-theme="dark">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App
